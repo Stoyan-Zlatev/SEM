806f662e5e48b53726fb63637a9835d15fd20f90
Tsvetilin Tsvetilov <tsvetilin.tsvetilov@gmail.com>
2023-10-18 19:49:23 +0300

    Add distribution functions tasks, solutions and cheatsheet

--- R/00.Cheatsheet.r
+++ R/00.Cheatsheet.r
@@ -1,8 +1,12 @@
+## General
+
 # x <- c(1,2,3) <=> x = c(1,2,3) - vector
 # 1:10 20:5 - range
 # seq (1, 10, 2) - sequence (start, finish, step)
 # rep(2, 10) - prints 2 10 times
 # rm(x) - deletes x
+
+## Simulations
 # sample(range, size, replace = F) - generates size random elements from range without repetitions
 # sample(vector, size, replace = T) - generates size random elements choosing from vector with repetitions
 # sample(vector, size, replace = T, prob = vector) - generates elements according to probability vector with same length
@@ -17,3 +21,27 @@
 # c(vector1, vector2) - concatenates the two vectors
 # c(1,2,3) %in% c(1,2,3) - returns true/false for each element if the elements of the first vector are in the second vector
 # which(vector == value) - returns the indexes where elements are equal to value
+
+
+
+## Distribution Functions
+
+# X = # of successes in n trials
+# p = probability of success in 1 trial
+# X ~ Bi(n,p)
+# P(X = k) = dbinom(k,n,p)
+# P(X <= k) = pbinom(k,n,p)
+ 
+ 
+# X = # trials until 1st success
+# p = probability of success in 1 trial
+# X ~ Ge(p)
+# P(X = k) = dgeom(k-1,p)
+# P(X <= k) = pgeom(k-1,p)
+ 
+ 
+# X = # trials until rth success
+# p = probability of success in 1 trial
+# X ~ NB(r,p)
+# P(X = k) = dnbinom(k-r,r,p)
+# P(X <= k) = pnbinom(k-r,r,p)
